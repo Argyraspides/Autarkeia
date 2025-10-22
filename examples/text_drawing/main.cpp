1725449f1a9df89646db946d02c53abcb01118d7
Argyraspides <joshivinayak441@gmail.com>
2025-10-22 21:35:36 +1300

    got some basic shiet

---
+++ examples/text_drawing/main.cpp
@@ -0,0 +1,19 @@
+#include "Frame.hpp"
+#include "Vec2I.hpp"
+
+#include "DrawUtils.hpp"
+#include "TextDrawUtils.hpp"
+
+#include <iostream>
+
+int main()
+{
+    Frame f( 25, 25 );
+
+    DrawUtils::Clear(f, '.');
+    DrawUtils::ClearScreen();
+    DrawUtils::DrawText( "A", { 10, 10 }, f, 'x' );
+    DrawUtils::Draw(f);
+
+    std::cin.get();
+}
--- modules/drawing/src/TextDrawUtils.cpp
+++ modules/drawing/src/TextDrawUtils.cpp
@@ -1,11 +1,92 @@
 #include "TextDrawUtils.hpp"
+#include "DrawUtils.hpp"
+#include "Frame.hpp"
 #include "Vec2I.hpp"
+#include <array>
+#include <utility>
+#include <unordered_map>
+
+enum CharBoxPosition
+{
+    TOP_LEFT,
+    TOP_RIGHT,
+
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT,
+
+    TOP_MIDDLE,
+    BOTTOM_MIDDLE,
+
+    LEFT_MIDDLE,
+    RIGHT_MIDDLE
+};
+
+static std::array< std::pair< CharBoxPosition, CharBoxPosition >, 4 > LETTER_A = { {
+    { CharBoxPosition::BOTTOM_LEFT, CharBoxPosition::TOP_LEFT },
+    { CharBoxPosition::BOTTOM_RIGHT, CharBoxPosition::TOP_RIGHT },
+    { CharBoxPosition::BOTTOM_RIGHT, CharBoxPosition::TOP_RIGHT },
+    { CharBoxPosition::BOTTOM_RIGHT, CharBoxPosition::TOP_RIGHT },
+} };
 
 namespace DrawUtils
 {
 
-void DrawText( const std::string& txt, Vec2I dim, Frame& frame, const char& drawChar )
+
+void _DrawCharacter( const std::array< std::pair< CharBoxPosition, CharBoxPosition >, 4 >& glyph,
+                     Vec2I dim,
+                     Frame& frame,
+                     const char& drawChar )
 {
+    Vec2I BOTTOM_LEFT = { 0, dim.y };
+    Vec2I BOTTOM_RIGHT = { dim.x, dim.y };
+
+    Vec2I TOP_LEFT = { 0, 0 };
+    Vec2I TOP_RIGHT = { dim.x, 0 };
+
+    Vec2I TOP_MIDDLE = { dim.x / 2, 0 };
+    Vec2I BOTTOM_MIDDLE = { dim.x / 2, dim.y };
+
+    Vec2I LEFT_MIDDLE = { 0, dim.y / 2 };
+    Vec2I RIGHT_MIDDLE = { dim.x, dim.y / 2 };
+
+    std::unordered_map< CharBoxPosition, Vec2I > charBoxToVec;
+    charBoxToVec[CharBoxPosition::BOTTOM_LEFT] = BOTTOM_LEFT; 
+    charBoxToVec[CharBoxPosition::BOTTOM_RIGHT] = BOTTOM_RIGHT; 
+
+    charBoxToVec[CharBoxPosition::TOP_LEFT] = TOP_LEFT; 
+    charBoxToVec[CharBoxPosition::TOP_RIGHT] = TOP_RIGHT; 
+
+    charBoxToVec[CharBoxPosition::TOP_MIDDLE] = TOP_MIDDLE; 
+    charBoxToVec[CharBoxPosition::BOTTOM_MIDDLE] = BOTTOM_MIDDLE; 
+    
+    charBoxToVec[CharBoxPosition::LEFT_MIDDLE] = LEFT_MIDDLE; 
+    charBoxToVec[CharBoxPosition::RIGHT_MIDDLE] = RIGHT_MIDDLE; 
+
+    for ( const std::pair< CharBoxPosition, CharBoxPosition >& line : glyph )
+    {
+        const Vec2I& p1 = charBoxToVec.at(line.first);
+        const Vec2I& p2 = charBoxToVec.at(line.second);
+        DrawUtils::DrawLine(p1, p2, frame, drawChar);
+    }
 }
 
-} // namespace
+void DrawCharacter( const char& ch, Vec2I dim, Frame& frame, const char& drawChar )
+{
+    switch ( ch )
+    {
+    case 'a':
+    case 'A': {
+        _DrawCharacter( LETTER_A, dim, frame, drawChar );
+    }
+    }
+}
+
+void DrawText( const std::string& txt, const Vec2I& dim, Frame& frame, const char& drawChar )
+{
+    for( const char& c : txt )
+    {
+        DrawCharacter(c, dim, frame, drawChar );
+    }
+}
+
+} // namespace DrawUtil
