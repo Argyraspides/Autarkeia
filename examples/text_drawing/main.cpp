ffde1dcc5277fa77e49c9f59c0e73de4a6d3d001
Argyraspides <joshivinayak441@gmail.com>
2025-10-26 01:53:57 +1300

    Can now render numbers

--- examples/text_drawing/main.cpp
+++ examples/text_drawing/main.cpp
@@ -4,16 +4,26 @@
 #include "DrawUtils.hpp"
 #include "TextDrawUtils.hpp"
 
+#include <chrono>
 #include <iostream>
+#include <string>
+#include <thread>
 
 int main()
 {
-    Frame f(25, 25);
+    Frame f( 100, 25 );
 
-    DrawUtils::Clear(f, ' ');
+    DrawUtils::Clear( f, ' ' );
     DrawUtils::ClearScreen();
-    DrawUtils::DrawText( "9", {10, 10 }, f, 'x' );
-    DrawUtils::Draw(f);
+
+    for ( int i = 0; i < 100; i++ )
+    {
+        DrawUtils::DrawText( std::to_string( i ), { 10, 10 }, f, 'x' );
+        DrawUtils::Draw( f );
+        std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+        DrawUtils::Clear( f, ' ' );
+        DrawUtils::ClearScreen();
+    }
 
     std::cin.get();
 }
